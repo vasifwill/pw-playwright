593793cf34d947d8e42d5119f33d6a98475792d5
Vasif Mammadov <vasif.will@gmail.com>
2024-07-19 16:52:39 -0700

    charts

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -1,7 +1,44 @@
 import {test} from '@playwright/test'
 
-test('the first test', async ({page}) => {
+
+test.beforeEach(async({page}) => {
     await page.goto('http://localhost:4200/')
-    await page.getByText('Forms').click()
-    await page.getByText('Form Layouts').click()
+    })
+
+
+
+
+
+//test.describe.skip 0r test.describe.only
+
+test.describe('suit 1',() => {
+    test.beforeEach(async({page}) => {
+        await page.getByText('Forms').click()
+    })
+
+
+    test('form layouts',async({page}) => {
+        await page.getByText('Form Layouts').click()
+    })
+
+    test('navigate to  datepicker', async ({page}) => {
+        await page.getByText('Datepicker').click()
+    })
+
+    
+
+})
+
+
+test.describe('navigate to charts',() => {
+    test.beforeEach(async({page}) => {
+        await page.getByText('Charts').click()
+    })
+    
+    
+    test('navigate to  echarts', async ({page}) => {
+        await page.getByText('Echarts').click()
+    })
+
+
 })
